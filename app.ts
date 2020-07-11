39bb707801bb8df2f37b53ecc7d3ac3a64faba45
Tri Ardini <tr.ardn@gmail.com>
2020-07-11 23:31:38 +0700

    add another route

--- app.ts
+++ app.ts
@@ -9,4 +9,7 @@ app.
     get("/about-me", (ctx: Context) => {
         ctx.json({"id": "123", "title": "about-me page"})
     }).
+    get("/hire-me", (ctx: Context) => {
+        return "PLS HIRE ME!!"
+    }).
     start({ port: 3400 })
