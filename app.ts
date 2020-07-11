4ce8a2ad2a9ad934d9a91f1cc818d6e18216441d
Tri Ardini <tr.ardn@gmail.com>
2020-07-11 23:30:01 +0700

    add new route

--- app.ts
+++ app.ts
@@ -1,9 +1,12 @@
-import { Application } from "https://deno.land/x/abc@v1/mod.ts";
+import { Application, Context } from "https://deno.land/x/abc@v1/mod.ts";
 
 const app = new Application()
 
 app.
     get("/", () => {
         return "Hi, this is index page"
+    }).
+    get("/about-me", (ctx: Context) => {
+        ctx.json({"id": "123", "title": "about-me page"})
     }).
     start({ port: 3400 })
